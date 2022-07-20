f91311e6b712283cfc7061d19baf35565f9b4655
onions127 <stavyanovski@gmail.com>
2022-07-20 23:41:23 +0300

    added stick and diamond stick

---
+++ src/main/java/net/Stagav/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.Stagav.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/Stagav/tutorialmod/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.Stagav.tutorialmod.item;
+
+import net.Stagav.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item STICK = registerItem("stick", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item DIAMOND_STICK = registerItem("diamond_stick", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+    private static Item registerItem(String name, Item item) {
+
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+
+    }
+
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.debug("registering Mod Items for " + TutorialMod.MOD_ID );
+    }
+
+
+}
