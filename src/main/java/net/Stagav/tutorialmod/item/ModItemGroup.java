e0af3cd3885282d519cb44b0461d86a5ceabe81c
Stagav <73000023+onions127@users.noreply.github.com>
2023-02-01 20:21:50 +0200

    renames

--- src/main/java/net/Stagav/tutorialmod/item/ModItemGroup.java
+++ src/main/java/net/Stagav/tutorialmod/item/ModItemGroup.java
@@ -8,6 +8,6 @@ import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
 
-    public static final ItemGroup DISCS = FabricItemGroupBuilder.build(new Identifier(TutorialMod.MOD_ID, "Discs"), () -> new ItemStack(ModItems.DISC_ONE));
+    public static final ItemGroup DISCS = FabricItemGroupBuilder.build(new Identifier(TutorialMod.MOD_ID, "discs"), () -> new ItemStack(ModItems.DISC_ONE));
 
 }
